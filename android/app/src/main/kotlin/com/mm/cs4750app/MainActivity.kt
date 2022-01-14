5b1b103a6d1e0296fe765cc25438872e36d1f4ee
matilda-minasian <matildam.1914@gmail.com>
2022-01-13 18:00:47 -0800

    My second commit

---
+++ android/app/src/main/kotlin/com/mm/cs4750app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mm.cs4750app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homescreen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Icon customIcon = const Icon(Icons.search);
+  Widget customSearchBar = const Text('My Personal Journal');
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: customSearchBar,
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                if (customIcon.icon == Icons.search) {
+                  customIcon = const Icon(Icons.cancel);
+                  customSearchBar = const ListTile(
+                    leading: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                      size: 28,
+                    ),
+                    title: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'type in book name...',
+                        hintStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontStyle: FontStyle.italic,
+                        ),
+                        border: InputBorder.none,
+                      ),
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  );                } else {
+                  customSearchBar = const Text('Elite Book Club');
+                  customIcon = const Icon(Icons.search);
+                }
+              });
+            },
+            icon: customIcon,
+          ),
+        ],
+        centerTitle: true,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.forum_rounded),
+            label: 'Forum',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.list_rounded),
+            label: 'My Lists',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle_rounded),
+            activeIcon: Icon(Icons.account_circle_rounded, color: Colors.orange),
+            label: 'Account',
+
+          ),
+        ],
+
+      ),
+    );
+  }
+}
